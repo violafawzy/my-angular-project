8907ad9df749634587d3c73f4824034bcbef8390
violafawzy <violafawzy855@gmail.com>
2024-09-15 23:17:51 +0300

    loginPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';  
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [LoginComponent]  
 })
 export class AppComponent {
-  title = 'my-angular-project';
-}
+  title = 'your-app';}
